3d896dca1080af0fb8b2416640d36946c3baa108
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-19 22:50:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get "students/:id", to: "students#show", as: "student"
    get 'students/:id/activate', to: 'students#activate', as:'activate-student'
 end
